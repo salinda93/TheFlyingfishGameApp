638acd59d747e3a81c8bc1ae28a281ad81b04a7f
Salinda <salinda.jayasundara3010@gmail,com>
2020-04-13 07:44:33 +0530

    Initial commit

---
+++ app/src/main/java/com/example/salinda/theflyingfishgameapp/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.salinda.theflyingfishgameapp;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    private flyingFishView gameView;
+    private Handler handler = new Handler();
+    private static final long Interval = 30;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        gameView = new flyingFishView(this);
+        setContentView(gameView);
+
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        gameView.invalidate();
+                    }
+                });
+            }
+        },0,Interval);
+    }
+}
